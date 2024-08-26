a8112ba63fec326553098dbbb720b934b48c20fb
jezura777 <68075487+jezura777@users.noreply.github.com>
2024-08-27 01:59:08 +0200

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
-VERSION   = 0
+VERSION  ?= 0
 PROG      = sgph
 
-CPPFLAGS  = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_XOPEN_SORCE=700 -D_BSD_SOURCE
+CPPFLAGS += -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_XOPEN_SORCE=700 -D_BSD_SOURCE
 CFLAGS   ?= -std=c99 -pedantic -Wall -Wextra -Os
-LDFLAGS  ?= -s
+LDFLAGS  += -s
 
 PREFIX   ?= /usr/local
 
